4cf9815ac520188ee235e98f16c86bdcd4ac9891
Ennio Gomide dos Santos <ennio.gomide@gmail.com>
2022-01-20 10:09:23 -0300

    Database seed

--- backend/src/main/java/com/devsuperior/dscatalog/services/CategoryService.java
+++ backend/src/main/java/com/devsuperior/dscatalog/services/CategoryService.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.devsuperior.dscatalog.entities.Category;
 import com.devsuperior.dscatalog.repositories.CategoryRepository;
@@ -17,9 +18,14 @@ public class CategoryService {
 	@Autowired
 	private CategoryRepository repository;
 	
+	/*
+	 * para controle de transação: no início coloca @Transactional (importar a do spring)
+	 * com readonly não trava o banco (não faz lock).
+	 * Todo o controle de entidade, resolve na camada de serviço.
+	 */
+	@Transactional(readOnly = true)
 	public List<Category> findAll() {
 		return repository.findAll();
-		
 	}
 
 }
