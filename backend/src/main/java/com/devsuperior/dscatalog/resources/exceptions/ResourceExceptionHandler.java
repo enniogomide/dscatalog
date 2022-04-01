23a0b211c707e3ac07cbb14ff4b1f93a464dfea0
Ennio Gomide dos Santos <ennio.gomide@gmail.com>
2022-04-01 16:16:38 -0300

    UserupdateValidator

--- backend/src/main/java/com/devsuperior/dscatalog/resources/exceptions/ResourceExceptionHandler.java
+++ backend/src/main/java/com/devsuperior/dscatalog/resources/exceptions/ResourceExceptionHandler.java
@@ -5,7 +5,6 @@ import java.time.Instant;
 
 import javax.servlet.http.HttpServletRequest;
 
-import org.springframework.boot.context.properties.bind.validation.ValidationErrors;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.FieldError;
